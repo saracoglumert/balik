d7c867a30efaa1f8af9b77c19cc892e62283b0f9
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2022-05-03 23:23:25 +0300

    Update init.bash

--- code/core/init.bash
+++ code/core/init.bash
@@ -1,5 +1,5 @@
 ## Configure OS ##
-# Install SSH Server
+#Install SSH Server
 sudo apt install openssh-server
 
 # Install Avahi
@@ -7,7 +7,10 @@ sudo apt install avahi-daemon
 
 ## Install ROS2
 # Add PGP Key
+sudo apt-key adv --fetch-keys https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc
 
 # Add Repo
+sudo apt-add-repository http://packages.ros.org/ros2/ubuntu
 
 # Install ROS2
+sudo apt install ros-foxy-ros-base
