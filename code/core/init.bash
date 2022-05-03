a34b9173e6b4bc0a1a854a587d8d772dabd1ff8f
saracoglumert <70754740+saracoglumert@users.noreply.github.com>
2022-05-03 23:20:24 +0300

    Create init.bash

---
+++ code/core/init.bash
@@ -0,0 +1,13 @@
+## Configure OS ##
+# Install SSH Server
+sudo apt install openssh-server
+
+# Install Avahi
+sudo apt install avahi-daemon
+
+## Install ROS2
+# Add PGP Key
+
+# Add Repo
+
+# Install ROS2
