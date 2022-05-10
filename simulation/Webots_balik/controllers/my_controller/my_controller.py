93af4a0ee1ba29a4aeaacf22dd0288386eae771e
moneylobster <87999452+moneylobster@users.noreply.github.com>
2022-05-11 01:17:36 +0300

    interface controller with sensors and motors

--- simulation/Webots_balik/controllers/my_controller/my_controller.py
+++ simulation/Webots_balik/controllers/my_controller/my_controller.py
@@ -1,8 +1,7 @@
 """my_controller controller."""
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
-from controller import Robot
+# import robot and actuator/sensor code
+from controller import Robot, Motor, DistanceSensor
 
 # create the Robot instance.
 robot = Robot()
@@ -10,11 +9,22 @@ robot = Robot()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
+
+#initialize sonar.
+#angle between two consecutive sensor directions is 20 degrees except for the four side sensors (so0, so7, so8 and so15) for which the angle is 40 degrees.
+so=[]
+soNames=[
+    'so0', 'so1', 'so2',  'so3',  'so4',  'so5',  'so6',  'so7', #front
+    'so8', 'so9', 'so10', 'so11', 'so12', 'so13', 'so14', 'so15' #back
+    ]
+
+for soName in soNames:
+    so.append(robot.getDevice(soName))
+    so[-1].enable(timestep)
+
+#initialize motors
+lm=robot.getDevice("left wheel")
+rm=robot.getDevice("right wheel")
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
@@ -22,19 +32,20 @@ while robot.step(timestep) != -1:
     # Read the sensors:
     # Enter here functions to read sensor data, like:
     #  val = ds.getValue()
+    reading=so[4].getValue()
 
     # Process sensor data here.
-    
+    print(reading)
 
     # Enter here functions to send actuator commands, like:
     #  motor.setPosition(10.0)
-    pass
+    lm.setPosition(10)
 
-# Enter here exit cleanup code.
 
 def bugnext(robot_coords, target_coords, readings):
-    #calculates the next step according to the bug algo
+    #calculates the next step according to the bug algo. In progress.
     #robot_coords: tuple (x,y)
     #target_coords: tuple (x,y)
     #readings: tuple ((r0,r1,r2,r3,r4,r5,r6,r7),(l0,l1,l2,l3,l4,l5,l6,l7))
+    pass
     
